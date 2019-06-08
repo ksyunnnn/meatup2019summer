22e537845b9f06e8902adaa1db7cf897e4a1fb07
ksyunnnn <is.syunsukekobashi@gmail.com>
2019-06-08 12:08:16 +0900

    ファーストビューまで

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,43 +6,128 @@ import Layout from '../components/layout';
 import OnikuImage from '../components/molecules/Images/OnikuImage';
 import SEO from '../components/seo';
 
+const orange = '#FF6500';
+
 const MainWrapper = styled.div`
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  padding-bottom: 176px;
+`;
 
-  /* pennging
-  .year {
-    background: #FF6500;
-    font-size: 85px;
-    line-height: 119.8%;
-    letter-spacing: -1.7px;
+const HeroSection = styled.section`
+  padding-top: 80px;
+  padding-bottom: 110px;
+  text-align: center;
+  h3 {
+    font-size: 16px;
+    margin-bottom: 40px;
+  }
+  h1 {
+    display: grid;
+    .wrapper {
+      display: float;
+      justify-content: center;
+    }
+    span.title {
+      font-size: 68px;
+    }
+  }
+  span.year {
+    background: ${orange};
+    color: #fff;
+    font-size: 40px;
+    padding: 0 8px;
+    margin-right: 16px;
+  }
+  .oniku {
+    width: 64px;
+    margin: auto;
+    padding: 16px 0;
+    padding-bottom: 28px;
+  }
+  .date {
+    font-size: 20px;
+    line-height: 28px;
+    margin-bottom: 48px;
+    span { display: block; }
+    .place {
+      padding-top: 1.5em;
+      i { 
+        margin-right: 8px;
+        color: ${orange};
+       }
+    }
+  }
+  .button-wrapper {
+    display: grid;
+    row-gap: 8px;
+    justify-content: center;
+  }
+`;
 
-    color: #FFFFFF;
-  } */
+const StyledButton = styled.button`
+  width: 300px;
+  height: 48px;
+  color: ${props => props.orange && orange};
+  font-weight: 800;
+  border: 3px solid;
+  border-radius: 24px;
+  background: #FFFFFF;
+  font-size: 16px;
+  :active {
+    background: #eee;
+  }
+`;
+
+const DetailSection = styled.section`
+  background: #F8F8F8;
+  padding-top: 74px;
+  text-align: center;
+  h2 {
+    font-size: 40px;
+    text-transform: uppercase;
+  }
 `;
 
 export default () => (
   <Layout>
     <SEO title="Home" />
     <MainWrapper>
-      <div className="contents">
-        {/* pennding
-        <span className="year">2019</span>
-        <span className="season">REIWA SUMMER</span>
-        <span className="oniku"><OnikuImage /></span>
-        <h1>MEATUP</h1> */}
-        <a href="https://twitter.com/hashtag/meatup2019" target="_blank" rel="noopener noreferrer">
-#meatup2019
-          <i className="fas fa-external-link-alt" style={{ marginLeft: '4px' }} />
-        </a>
-        <OnikuImage />
-        <div style={{ textAlign: 'center', fontSize: '14px', letterSpacing: '4px' }}>
-        準備中です
-          <span role="img" aria-label="bow">🙇</span>
+      <HeroSection>
+        <h3>
+          参加者募集開始〜
+          <span role="img" aria-label="tada">🎉</span>
+        </h3>
+        <h1>
+          <div className="wrapper">
+            <span className="year">2019</span>
+            <span>REIWA SUMMER</span>
+          </div>
+          <span className="title">MEATUP</span>
+        </h1>
+        <div className="oniku"><OnikuImage /></div>
+        <div className="date">
+          <span>2019.07.20(Sat.)</span>
+          <span>11:00 open ~ 19:00 close </span>
+          <span className="place">
+            <i className="fas fa-map-marker-alt" />
+            EAT TOKYO STUDIO JAX
+          </span>
         </div>
-      </div>
+        <div className="button-wrapper">
+          <StyledButton orange onClick={() => console.log('hoge')}>
+            <i className="fas fa-angle-double-down" style={{ marginRight: '8px' }} />
+            はよ参加登録させてくれ
+          </StyledButton>
+          <StyledButton onClick={() => console.log('hoge')}>
+            <i className="fas fa-angle-double-down" style={{ marginRight: '8px' }} />
+            まずは内容を見せて欲しい
+          </StyledButton>
+        </div>
+      </HeroSection>
+
+      <DetailSection>
+        <h2>About MEATUP</h2>
+        <p>肉を通してわいがや交流するイベントです</p>
+      </DetailSection>
     </MainWrapper>
   </Layout>
 );
