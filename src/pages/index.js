3b7cc0dbf851d40308c0920a26b4259230845bd0
ksyunnnn <is.syunsukekobashi@gmail.com>
2019-06-18 19:58:20 +0900

    おおまかな実装

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { Link } from 'gatsby';
 
@@ -11,12 +12,65 @@ import Oniku02 from '../components/molecules/Images/Oniku02';
 import Atom01 from '../components/molecules/Images/Atom01';
 import Atom02 from '../components/molecules/Images/Atom02';
 import Forms from '../components/organisms/Forms';
-import DataBox from '../components/molecules/DataBox';
+import CardStyle from '../components/molecules/CardStyle';
 
 import { colors } from '../helpers/State';
 
+const copyToClipboard = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  let isSafari = false;
+
+  const textareaElement = document.createElement('textarea');
+
+  if (userAgent.indexOf('msie') !== -1 || userAgent.indexOf('trident') !== -1) {
+    // console.log('Internet Explorerをお使いですね');
+  } else if (userAgent.indexOf('edge') !== -1) {
+    // console.log('Edgeをお使いですね');
+  } else if (userAgent.indexOf('chrome') !== -1) {
+    // console.log('Google Chromeをお使いですね');
+  } else if (userAgent.indexOf('safari') !== -1) {
+    // console.log('Safariをお使いですね');
+    isSafari = true;
+  } else if (userAgent.indexOf('firefox') !== -1) {
+    // console.log('FireFoxをお使いですね');
+  } else if (userAgent.indexOf('opera') !== -1) {
+    // console.log('Operaをお使いですね');
+  } else {
+    // console.log('そんなブラウザは知らん');
+  }
+
+
+  textareaElement.value = `
+  ･名前
+
+  ･性別
+
+  ･Web歴
+    未経験 / 1年未満 / 2年~4年 / 5年~
+  ･職域
+    フロントエンド / バックエンド / デザイン / ディレクター / 学生 / その他
+  ･好きな肉の部位
+
+  ･肉に一番合うもの
+
+  `;
+  document.body.appendChild(textareaElement);
+
+  if (isSafari) {
+    const range = document.createRange();
+    range.selectNode(textareaElement);
+    window.getSelection().addRange(range);
+  } else {
+    textareaElement.select();
+  }
+
+
+  document.execCommand('copy');
+  textareaElement.parentElement.removeChild(textareaElement);
+};
+
 const MainWrapper = styled.div`
-  padding-bottom: 176px;
+  padding-bottom: 120px;
 `;
 
 const HeroSection = styled.section`
@@ -26,6 +80,7 @@ const HeroSection = styled.section`
   h3 {
     font-size: 16px;
     margin-bottom: 40px;
+    font-weight: bold;
   }
   h1 {
     display: grid;
@@ -89,35 +144,38 @@ const DetailSection = styled.section`
   padding-top: 74px;
   background: ${colors.gray};
   text-align: center;
-  h2 {
-    font-size: 40px;
-    text-transform: uppercase;
-    margin-bottom: 24px;
-  }
-  h3 {
-    font-weight: 900;
-    font-size: 16px;
-    line-height: 25px;
-    margin-bottom: 40px;
-  }
+
+  overflow: hidden;
+
   .photo-list {
     display: grid;
     row-gap: 24px;
-    margin-bottom: 80px;
+    margin-bottom: 100px;
     > * {
       border: 5px solid ${colors.white};
     }
   }
-  .data-list {
-    padding-bottom: 50px;
-    li {
-      list-style: none;
+
+  .schedule {
+    margin-bottom: 100px;
+    ul {
+      display: grid;
+      row-gap: 16px;
     }
-    h4 {
+    li {
+      letter-spacing: 1px;
+    }
+  }
+
+  .data-list {
+    margin-bottom: 100px;
+    padding-bottom: 50px;
+    p.pre {
       font-weight: 900;
-      font-size: 24px;
-      line-height: 30px;
+      font-size: 20px;
       letter-spacing: 1.46px;
+      color: #B1B1B1;
+      margin-top: 40px;
     }
   }
 `;
@@ -125,6 +183,43 @@ const DetailSection = styled.section`
 const FormSection = styled.section`
   padding: 0 40px;
   padding-top: 74px;
+  h4 { margin-bottom: 16px; }
+  p {
+    line-height: 2em;
+  }
+  ul.steps {
+    display: grid;
+    row-gap: 40px;
+  }
+  ul.questionnaire {
+    padding: 16px 0;
+    display: grid;
+    row-gap: 4px;
+    font-size: 11px;
+    letter-spacing: 1px;
+    margin-left: 24px;
+    li { list-style: disc; }
+  }
+  button.copy-button {
+    padding: 4px;
+    border: 1px;
+    outline-width: medium;
+    background: #fff;
+    margin-bottom: 24px;
+  }
+`;
+
+const ShareSection = styled.section`
+  background: ${colors.gray};
+  padding: 80px 40px;
+  font-weight: 800;
+  font-size: 20px;
+  line-height: 25px;
+`;
+
+const Footer = styled.footer`
+  padding: 40px 16px;
+  text-align: center;
 `;
 
 export default () => (
@@ -147,6 +242,7 @@ export default () => (
         <div className="date">
           <span>2019.07.20(Sat.)</span>
           <span>11:00 open ~ 19:00 close </span>
+          <span>entry ¥4,000</span>
           <span className="place">
             <i className="fas fa-map-marker-alt" />
             EAT TOKYO STUDIO JAX
@@ -174,10 +270,47 @@ export default () => (
           <Atom02 />
         </div>
 
+        <h2>Schedule</h2>
+        <div className="schedule">
+          <CardStyle>
+            <ul>
+              <h4>肉。</h4>
+              <li>
+               11:00~ : 開場
+              </li>
+              <li>
+                12:00~ : 乾杯して肉食う
+              </li>
+              <li>
+               12:00~13:00 : 肉料理 by
+                {' '}
+                <a href="https://twitter.com/naoki_ffc_96" target="_blank" rel="noopener noreferrer">naoki</a>
+              </li>
+              <li>
+               13:00~15:00 :
+                {' '}
+                <a href="https://twitter.com/search?q=%23meatup2019%20%E3%81%AA%E3%81%AB%E3%81%8B%E4%BC%81%E7%94%BB" target="_blank" rel="noopener noreferrer">なにか企画</a>
+              </li>
+              <h4 style={{ marginTop: '16px' }}>交流。</h4>
+              <li>
+               15:00~18:00 :
+                {' '}
+                <a href="https://twitter.com/search?q=%23meatup2019%20%E9%9F%B3%E6%A5%BD%E7%B3%BB%E3%81%AE%E3%81%AA%E3%81%AB%E3%81%8B" target="_blank" rel="noopener noreferrer">音楽系のなにか</a>
+              </li>
+              <li>
+               18:00 : 集合写真とか撮りたい
+              </li>
+              <li>
+               18:00~19:00 : みんなで撤収
+              </li>
+            </ul>
+          </CardStyle>
+        </div>
+
         <h2>
-          MEMBER
+          Member
           <br />
-          DATA
+          Data
         </h2>
         <h3>
           現在受付けている参加者データです
@@ -185,30 +318,130 @@ export default () => (
         </h3>
         <ul className="data-list">
           <li>
-            <DataBox>
+            <CardStyle>
+              <h4>参加人数</h4>
+              <p className="pre">準備中...</p>
+            </CardStyle>
+          </li>
+          <li>
+            <CardStyle>
               <h4>男女比</h4>
-            </DataBox>
+              <p className="pre">準備中...</p>
+            </CardStyle>
           </li>
           <li>
-            <DataBox>
+            <CardStyle>
               <h4>Web歴</h4>
-            </DataBox>
+              <p className="pre">準備中...</p>
+            </CardStyle>
           </li>
           <li>
-            <DataBox>
+            <CardStyle>
               <h4>職業マップ</h4>
+              <p className="pre">準備中...</p>
               {/*
                 [TODO]このイメージ
                 https://images.app.goo.gl/BuCu2w8DXe1ZGQpW6
                */}
-            </DataBox>
+            </CardStyle>
           </li>
         </ul>
       </DetailSection>
-      {/* WIP <FormSection>
+      <FormSection id="joinform">
         <h2>JOINFORM</h2>
-        <Forms />
-      </FormSection> */}
+        {/* <Forms /> */}
+        <div>
+          <ul className="steps">
+            <li>
+              <h4>1. 運営へ連絡</h4>
+              <p>
+                こばしゅん(
+                {' '}
+                <a href="https://twitter.com/ksyunnnn" target="_blank" rel="noopener noreferrer">Twitter</a>
+                 /
+                {' '}
+                <a href="https://m.me/ksyunnnn" target="_blank" rel="noopener noreferrer">Facebook</a>
+                {' '}
+                )まで！
+              </p>
+              <p>
+                以下を明記ください！アンケートも兼ねてます
+                <span role="img" aria-label="bow">🙇</span>
+              </p>
+              <ul className="questionnaire">
+                <li>名前</li>
+                <li>性別</li>
+                <li>Web歴: 未経験 / 1年未満 / 2年~4年 / 5年~</li>
+                <li>職域: フロントエンド / バックエンド / デザイン / ディレクター / 学生 / その他</li>
+                <li>好きな肉の部位</li>
+                <li>肉に一番合うもの</li>
+              </ul>
+              <button
+                onClick={copyToClipboard}
+                type="button"
+                className="copy-button"
+              >
+                クリップボードにコピー
+                <i className="far fa-copy" />
+              </button>
+              <p>
+                <b>連絡先</b>
+                :
+                {' '}
+                <a href="https://twitter.com/ksyunnnn" target="_blank" rel="noopener noreferrer">Twitter</a>
+                 /
+                {' '}
+                <a href="https://m.me/ksyunnnn" target="_blank" rel="noopener noreferrer">Facebook</a>
+
+              </p>
+            </li>
+            <li>
+              <h4>2. 返信を待つ</h4>
+              <p>
+                2-3日以内に返信なければ、もう一度連絡ください
+                <span role="img" aria-label="bow">🙇</span>
+              </p>
+            </li>
+            <li>
+              <h4>3. 参加費を払う</h4>
+              <p>
+                事前決済をお願いしています！
+                <a href="https://twitter.com/search?q=%23meatup2019%20支払い" target="_blank" rel="noopener noreferrer">集金アプリKyash</a>
+                を利用する予定です。
+              </p>
+              <p>
+                当日の支払いも対応可能ですので、ご相談ください〜！
+                <span role="img" aria-label="hand">✌</span>
+              </p>
+            </li>
+            <li>
+              <h4>4. 予定をカレンダーに追加</h4>
+              <p>
+                <a target="_blank" rel="noopener noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MW5mcTlrbmJ1ZGZsYmt1dWo0MG5qczYyaW4gZnBvbGdxZ2wzYmdlcnE2NTJzaHIwaG9uc2NAZw&amp;tmsrc=fpolgqgl3bgerq652shr0honsc%40group.calendar.google.com">こちら</a>
+                のリンクよりカレンダーに追加できるので、ぜひご利用ください〜！
+              </p>
+            </li>
+            <li>
+              <h4>5. 当日肉を食う！交流する！</h4>
+              <p>
+                お待ちしています
+                <span role="img" aria-label="niku">🔥🍖🍖🔥</span>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </FormSection>
     </MainWrapper>
+    <ShareSection>
+      <a style={{ color: colors.orange }} href="https://twitter.com/hashtag/meatup2019" target="_blank" rel="noopener noreferrer">#meatup2019</a>
+      {' '}
+        ハッシュタグを使って
+        Twitterを盛り上げよう！
+    </ShareSection>
+    <Footer>
+      オール準備 by
+      {' '}
+      <a href="https://twitter.com/hashtag/meatup2019実行委員会" target="_blank" rel="noopener noreferrer">meatup2019実行委員会</a>
+    </Footer>
   </Layout>
 );
